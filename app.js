ffc9b96a1ae1eae873c88f689b6864c3099ea371
B-slang <entr.brijesh@gmail.com>
2021-01-12 11:46:03 +0530

    functions working

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-//Todo 
-//
+const button = document.querySelector("button");
+const output = document.querySelector(".output");
+console.log(button);// check
+
+button.addEventListener("click",function() {
+    const cost = document.querySelector("input");
+        // console.log(cost.value * 0.15);
+        let tip = (cost.value * 0.15).toFixed(2);
+        output.innerText = `<h1> you should tip $ ${tip} on $ ${cost.value}</h1>`;
+    }); 
