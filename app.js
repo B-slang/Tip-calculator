34d7029d4bdc6e1a140f0b9e77c98fee161bec09
B-slang <entr.brijesh@gmail.com>
2021-01-12 12:41:01 +0530

    completed

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log(button);// check
 
 button.addEventListener("click",function() {
     const cost = document.querySelector("input");
-        // console.log(cost.value * 0.15);
+         console.log(cost.value * 0.15);
         let tip = (cost.value * 0.15).toFixed(2);
         output.innerText = `<h1> you should tip $ ${tip} on $ ${cost.value}</h1>`;
     }); 
